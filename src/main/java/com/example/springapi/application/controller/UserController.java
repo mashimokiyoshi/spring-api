c1bb90fbafd5cb609926e0b452d19376c3360a6b
真下清 <mashimo.kiyoshi>
2019-10-01 23:25:50 +0900

    api info

--- src/main/java/com/example/springapi/application/controller/UserController.java
+++ src/main/java/com/example/springapi/application/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.springapi.application.controller;
 
+import com.example.springapi.application.resource.ErrorResponse;
 import com.example.springapi.application.resource.UserBody;
 import com.example.springapi.domain.object.User;
 import com.example.springapi.domain.service.UserService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,6 +29,10 @@ public class UserController {
      * @param id 検索したいユーザID
      * @return ユーザ
      */
+    @ApiResponses({
+        @ApiResponse(code = 404, message = "Not Found", response = ErrorResponse.class),
+        @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
+    })
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
     public User findById(@PathVariable("id") String id) {
@@ -38,6 +45,10 @@ public class UserController {
      * @param userBody リクエストボディ
      * @return 更新後のユーザ
      */
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "Bad Request", response = ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User save(@RequestBody @Validated UserBody userBody) {
@@ -49,6 +60,10 @@ public class UserController {
      *
      * @param id 削除したいユーザID
      */
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Not Found", response = ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResponse.class),
+    })
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable("id") String id) {
